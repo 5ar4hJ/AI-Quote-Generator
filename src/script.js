8f5f2851694a96f761bd6b616f42597b711a9e18
5ar4hJ <busybeesjn@gmail.com>
2024-04-20 22:44:30 -0600

    Added basic form handling and Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings:
+      "Books give a soul to the universe, wings to the mind, flight to the imagination, and life to everything.",
+    autoStart: true,
+    cursor: null,
+    delay: 35,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
